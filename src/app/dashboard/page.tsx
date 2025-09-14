580d97834b6161345369fb2032db0ccd3774e624
Hamid Barfi <barfiabdolhamid@gmail.com>
2025-09-15 00:58:36 +0330

    feat: add login and dashboard pages skeleton

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Dashboard: NextPage = () => {
+  return (
+    <div className="">
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
